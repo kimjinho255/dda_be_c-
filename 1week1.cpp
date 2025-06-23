97ea753d0c34cbc03a8929ac2ca45ddc4f912ca2
kimjinho255 <jinho040905@gmail.com>
2025-06-24 06:16:38 +0900

    no message

--- 1week1.cpp
+++ 1week1.cpp
@@ -1,3 +1,12 @@
-//
-// Created by jinho on 25. 6. 24.
-//
+#include <iostream>
+using namespace std;
+int addTwoNum(int num1, int num2){
+  int sum = num1 + num2;
+  return sum;
+}
+int main(){
+  int a,b;
+  cin>>a>>b;
+ cout<<addTwoNum(a, b)<<endl;
+
+}
